9edd9666f9dad07c0f7492f0621bf9cac0b2c62e
i-zanis <ioanlafazanis@gmail.com>
2022-12-28 22:42:29 +0000

    initial commit

---
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/WardrobeBackendApplication.java
@@ -0,0 +1,13 @@
+package io.noblackhole.wardrobe.wardrobebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WardrobeBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/noblackhole/wardrobe/wardrobebackend/WardrobeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.noblackhole.wardrobe.wardrobebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
