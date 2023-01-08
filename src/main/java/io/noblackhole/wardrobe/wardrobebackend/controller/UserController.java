478127d9ed2a39acee16aaebdf5bce0efe6a975b
i-zanis <ioanlafazanis@gmail.com>
2023-01-08 20:55:55 +0000

    feat: add validation and tests to UserController

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/controller/UserController.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/controller/UserController.java
@@ -6,16 +6,19 @@ import io.noblackhole.wardrobe.wardrobebackend.service.UserService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-  private static final String RETRIEVING_ALL_USERS = "Retrieving all users from the database";
+  private static final String RETRIEVING_ALL_USERS = "Retrieving all users from the DB";
   private static final String RETRIEVING_USER_BY_ID = "Retrieving user with id: {}";
   private static final String REGISTERING_USER = "Registering user: {}";
 
@@ -26,12 +29,21 @@ public class UserController {
   }
 
   @GetMapping("/all")
-  public ResponseEntity<Iterable<User>> findAll() {
-    logger.info(RETRIEVING_ALL_USERS);
+  public ResponseEntity<List<User>> findAll() {
+    logger.info("Received request to get all users");
     try {
-      Iterable<User> users = userService.findAll();
+      List<User> users = userService.findAll();
+      if (users == null || users.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(null);
+      }
       return ResponseEntity.ok(users);
+    } catch (DataAccessException e) {
+      logger.error("Error accessing database", e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(null);
     } catch (Exception e) {
+      logger.error("Error getting all users", e);
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(null);
     }
@@ -39,52 +51,36 @@ public class UserController {
 
   @GetMapping("/{id}")
   public ResponseEntity<User> findById(@PathVariable Long id) {
-    logger.info(RETRIEVING_USER_BY_ID, id);
+    logger.info("Received request to get user with id {}", id);
     try {
       User user = userService.findById(id);
       return ResponseEntity.ok(user);
     } catch (Exception e) {
+      logger.error("Error getting user with id {}", id, e);
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(null);
     }
   }
 
   @PostMapping("/register")
-  public ResponseEntity<User> register(@Valid @RequestBody User user) {
+  public ResponseEntity<User> register(@Valid @RequestBody User user, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(null);
+    }
     try {
       User savedUser = userService.save(user);
-      return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+      return ResponseEntity.status(HttpStatus.CREATED)
+        .body(savedUser);
     } catch (Exception e) {
       if (e instanceof DuplicateEmailException) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+          .build();
       } else {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .build();
       }
     }
   }
 
-
 }
-//  @PutMapping("/{id}/preferences")
-//  public User updatePreferences(@PathVariable Long id, @RequestBody Preferences preferences) {
-//    logger.info("Updating preferences for user with id: " + id);
-//    return userService.updatePreferences(id, preferences);
-//  }
-
-//  @PostMapping("/login")
-//  public String login(@RequestBody LoginRequest request) {
-//    logger.info("Logging in user: " + request.getEmail());
-//    User user = userService.findByEmail(request.getEmail());
-//    if (user != null && passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-//      return jwtUtil.generateToken(user);
-//    }
-//    return userService.login(request);
-//  }
-
-// TODO register
-// TODO login
-// TODO user preferences
-
-//create mappings for login, register, and preferences
-
-
--- src/test/java/io/noblackhole/wardrobe/wardrobebackend/controller/UserControllerTest.java
+++ src/test/java/io/noblackhole/wardrobe/wardrobebackend/controller/UserControllerTest.java
@@ -1,48 +1,98 @@
 package io.noblackhole.wardrobe.wardrobebackend.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-
 import io.noblackhole.wardrobe.wardrobebackend.domain.User;
-import io.noblackhole.wardrobe.wardrobebackend.exception.UserServiceException;
+import io.noblackhole.wardrobe.wardrobebackend.repository.UserRepository;
+import io.noblackhole.wardrobe.wardrobebackend.service.UserService;
+import io.noblackhole.wardrobe.wardrobebackend.service.UserServiceImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import io.noblackhole.wardrobe.wardrobebackend.controller.UserController;
-import io.noblackhole.wardrobe.wardrobebackend.service.UserService;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
-@ExtendWith(MockitoExtension.class)
-class UserControllerTest {
-  @Mock
-  private UserService userService;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-  @InjectMocks
-  private UserController userController;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UserControllerTest {
+  private final TestRestTemplate testRestTemplate = new TestRestTemplate();
+  UserService userService;
+  @Autowired
+  UserController userController;
+  User user1 = new User.Builder().withId(1L)
+    .withFirstName("John")
+    .withLastName("Doe")
+    .withEmail("johndoe@gmail.com")
+    .withPassword("john-pass123")
+    .build();
+  User user2 = new User.Builder().withId(2L)
+    .withFirstName("Jane")
+    .withLastName("Doe")
+    .withEmail("janedoe@gmail.com")
+    .withPassword("jane-pass123")
+    .build();
+  @MockBean
+  private UserRepository userRepository;
+  @LocalServerPort
+  private int port;
+  @Autowired
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+  private MockMvc mockMvc;
 
   @BeforeEach
   void setUp() {
+    userService = new UserServiceImpl(userRepository);
+    userController = new UserController(userService);
+  }
+
+
+  @Test
+  public void findById_shouldReturnUser_Without_Password() throws Exception {
+    User expectedUser = user1;
+    given(userRepository.findById(expectedUser.getId())).willReturn(Optional.of(expectedUser));
+    MockHttpServletRequestBuilder request = get("/v1/users/{id}", expectedUser.getId());
+    mockMvc.perform(request)
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.id", is(1)))
+      .andExpect(jsonPath("$.firstName", is("John")))
+      .andExpect(jsonPath("$.lastName", is("Doe")))
+      .andExpect(jsonPath("$.email", is("johndoe@gmail.com")))
+      .andExpect(jsonPath("$.password").doesNotExist());
+    ;
+  }
+
+
+  @Test
+  void register() {
+  }
+
+  @AfterEach
+  void tearDown() {
   }
 
   @Test
-  void findAll() throws UserServiceException {
-    // Set up the mock service to return a list of users
-    List<User> expectedUsers = Arrays.asList(new User(), new User());
-    when(userService.findAll()).thenReturn(Optional.of(expectedUsers));
+  void testFindAll() {
+  }
 
-    // Call the controller's findAll method and verify it returns the expected list
-    List<User> returnedUsers = (List<User>) userController.findAll();
-    assertEquals(expectedUsers, returnedUsers);
+  @Test
+  void testFindById() {
+  }
 
-    // Verify that the service's findAll method was called
-    verify(userService).findAll();
+  @Test
+  void testRegister() {
   }
 }
