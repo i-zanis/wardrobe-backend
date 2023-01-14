ba8f8f75d05b2b5815db18f038fae1d6391f9494
i-zanis <ioanlafazanis@gmail.com>
2023-01-14 09:56:05 +0000

    refactor: Bootstrap data

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/bootstrap/BootStrapData.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/bootstrap/BootStrapData.java
@@ -1,40 +1,99 @@
 package io.noblackhole.wardrobe.wardrobebackend.bootstrap;
 
 import io.noblackhole.wardrobe.wardrobebackend.domain.User;
+import io.noblackhole.wardrobe.wardrobebackend.exception.UserNotFoundException;
+import io.noblackhole.wardrobe.wardrobebackend.exception.UserServiceException;
 import io.noblackhole.wardrobe.wardrobebackend.repository.UserRepository;
+import io.noblackhole.wardrobe.wardrobebackend.service.UserService;
+import io.noblackhole.wardrobe.wardrobebackend.service.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by jt on 12/23/19.
- */
 @Component
 public class BootStrapData implements CommandLineRunner {
-
+  private static final Logger logger = LoggerFactory.getLogger(BootStrapData.class);
   UserRepository userRepository;
+  UserService userService;
 
-  public BootStrapData(UserRepository userRepository) {
+  public BootStrapData(UserRepository userRepository, UserService userService) {
     this.userRepository = userRepository;
+    this.userService = new UserServiceImpl(userRepository);
+
   }
 
   @Override
-    public void run(String... args) {
-        System.out.println("Started in Bootstrap");
-        User user = new User();
-        user.setFirstName("John");
-        user.setLastName("Doe");
-        user.setEmail("f@f");
-        user.setPassword("f");
-        userRepository.save(user);
-        User user2 = new User();
-        user2.setEmail("rwr");
-        user2.setFirstName("rwr");
-        user2.setLastName("wrw");
-        userRepository.save(user2);
-    System.out.println("REPO COUNT: " + userRepository.count());
-    List<User> users = (List<User>) userRepository.findAll();
-    System.out.println(users);
+  public void run(String... args) throws UserServiceException, UserNotFoundException {
+    logger.info("Loading bootstrap data");
+    List<User> users = new ArrayList<>();
+    try {
+      users = userService.findAll();
+    } catch (UserNotFoundException e) {
+      logger.error("No users found in the DB");
     }
+    if (users.isEmpty()) getUsers();
+    ;
+    logger.info("Bootstrap data loaded");
+    logger.info("-------------------------------");
+
+  }
+
+  private void getUsers() throws UserServiceException, UserNotFoundException {
+    User user1 = new User.Builder()
+      .withId(1L)
+      .withFirstName("John")
+      .withLastName("Doe")
+      .withEmail("johndoe@gmail.com")
+      .withPassword("password")
+      .build();
+
+    User user2 = new User.Builder()
+      .withId(2L)
+      .withFirstName("Jane")
+      .withLastName("Doe")
+      .withEmail("janedoe@gmail.com")
+      .withPassword("password")
+      .build();
+
+    User user3 = new User.Builder()
+      .withFirstName("John")
+      .withLastName("Smith")
+      .withEmail("johnsmith@gmail.com")
+      .withPassword("password")
+      .build();
+
+    User user4 = new User.Builder()
+      .withFirstName("Jane")
+      .withLastName("Smith")
+      .withEmail("janesmith@gmail.com")
+      .withPassword("password")
+      .build();
+
+    userService.save(user1);
+    userService.save(user2);
+    userService.save(user3);
+    userService.save(user4);
+
+
+    List<User> users = userService.findAll();
+    logger.info("Users found: " + users.size());
+    logger.info("-------------------------------");
+    assert users.size() == 4;
+    assert users.get(0)
+      .getFirstName()
+      .equals("John");
+    assert users.get(0)
+      .getLastName()
+      .equals("Doe");
+    assert users.get(0)
+      .getEmail()
+      .equals("johndoe@gmail.com");
+    assert users.get(0)
+      .getPassword()
+      .equals("password");
+  }
 }
