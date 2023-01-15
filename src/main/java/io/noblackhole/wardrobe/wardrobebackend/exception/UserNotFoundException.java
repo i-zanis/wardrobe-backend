14b678880c125ff0d67c3bd78628832086ca3ad8
i-zanis <ioanlafazanis@gmail.com>
2023-01-15 21:12:50 +0000

    feat: add userNotFoundException class

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/exception/UserNotFoundException.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/exception/UserNotFoundException.java
@@ -1,4 +1,25 @@
 package io.noblackhole.wardrobe.wardrobebackend.exception;
 
 public class UserNotFoundException extends Exception {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+
+  public UserNotFoundException() {
+  }
+
+  public UserNotFoundException(String format, UserNotFoundException e) {
+  }
+
+  public UserNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public UserNotFoundException(Throwable cause) {
+    super(cause);
+  }
+
+  protected UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
 }
