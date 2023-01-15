a809354a509438f1cec472f1d67f19d6d96ee5cc
i-zanis <ioanlafazanis@gmail.com>
2023-01-15 22:28:42 +0000

    refactor: add Validation messages to Person

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Person.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Person.java
@@ -10,8 +10,10 @@ public class Person extends BaseEntity {
   @Column(name = "first_name")
   @NotBlank(message = "First name is required")
   private String firstName;
+  @NotBlank(message = "Last name is required")
   @Column(name = "last_name")
   private String lastName;
+  @NotBlank(message = "Email is required")
   @Email(message = "Email must be valid")
   private String email;
 
