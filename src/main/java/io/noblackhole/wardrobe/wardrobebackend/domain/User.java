1ffa78d6187d3b29a08528b923ec796501407f7d
i-zanis <ioanlafazanis@gmail.com>
2023-01-15 22:29:15 +0000

    fix: circular reference on requests on User

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/User.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/User.java
@@ -1,6 +1,9 @@
 package io.noblackhole.wardrobe.wardrobebackend.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.ArrayList;
@@ -11,8 +14,10 @@ import java.util.List;
 public class User extends Person {
   @OneToOne
   private Preferences preferences;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private String password;
-  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.EAGER)
+  @JsonManagedReference
   private List<Item> items = new ArrayList<>();
 
   public User() {
@@ -47,59 +52,82 @@ public class User extends Person {
   }
 
   public List<Item> getItems() {
+//     TODO check if it needs removing after tests pass
+    if (items == null) {
+      items = new ArrayList<>();
+    }
     return items;
+  }
+
+  public void setItems(List<Item> items) {
+    this.items = items;
   }
 
   public void addItem(Item item) {
     getItems().add(item);
   }
 
-  public static final class UserBuilder {
-    private String firstName;
+
+  public static final class Builder {
+    private Long id;
+    private @NotBlank String firstName;
     private String lastName;
-    private String email;
+    private @Email String email;
     private Preferences preferences;
     private String password;
+    private List<Item> items;
 
-    private UserBuilder() {
+    public Builder() {
     }
 
-    public static UserBuilder anUser() {
-      return new UserBuilder();
+    public static Builder anUser() {
+      return new Builder();
     }
 
-    public UserBuilder withFirstName(String firstName) {
+    public Builder withId(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder withFirstName(String firstName) {
       this.firstName = firstName;
       return this;
     }
 
-    public UserBuilder withLastName(String lastName) {
+    public Builder withLastName(String lastName) {
       this.lastName = lastName;
       return this;
     }
 
-    public UserBuilder withEmail(String email) {
+    public Builder withEmail(String email) {
       this.email = email;
       return this;
     }
 
-    public UserBuilder withPreferences(Preferences preferences) {
+    public Builder withPreferences(Preferences preferences) {
       this.preferences = preferences;
       return this;
     }
 
-    public UserBuilder withPassword(String password) {
+    public Builder withPassword(String password) {
       this.password = password;
+      return this;
+    }
+
+    public Builder withItems(List<Item> items) {
+      this.items = items;
       return this;
     }
 
     public User build() {
       User user = new User();
+      user.setId(id);
       user.setFirstName(firstName);
       user.setLastName(lastName);
       user.setEmail(email);
       user.setPreferences(preferences);
       user.setPassword(password);
+      user.setItems(items);
       return user;
     }
   }
