577f76032830e6a86e7106af7c4af7b4f633255f
i-zanis <ioanlafazanis@gmail.com>
2023-01-15 22:21:38 +0000

    test: add Integration get request for all users

--- src/test/java/io/noblackhole/wardrobe/wardrobebackend/integration/IntegrationTest.java
+++ src/test/java/io/noblackhole/wardrobe/wardrobebackend/integration/IntegrationTest.java
@@ -1,10 +1,15 @@
-package io.noblackhole.wardrobe.wardrobebackend.controller;
+package io.noblackhole.wardrobe.wardrobebackend.integration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.noblackhole.wardrobe.wardrobebackend.bootstrap.BootStrapData;
+import io.noblackhole.wardrobe.wardrobebackend.controller.UserController;
 import io.noblackhole.wardrobe.wardrobebackend.domain.User;
+import io.noblackhole.wardrobe.wardrobebackend.exception.GlobalExceptionHandler;
+import io.noblackhole.wardrobe.wardrobebackend.repository.ItemRepository;
 import io.noblackhole.wardrobe.wardrobebackend.repository.UserRepository;
+import io.noblackhole.wardrobe.wardrobebackend.service.ItemService;
 import io.noblackhole.wardrobe.wardrobebackend.service.UserService;
 import io.noblackhole.wardrobe.wardrobebackend.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,21 +20,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 public class IntegrationTest {
   private static final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
-
   @Autowired
   UserRepository userRepository;
   @Autowired
@@ -39,15 +47,20 @@ public class IntegrationTest {
   MockMvc mockMvc;
   User user1, user2;
   ObjectMapper objectMapper = new ObjectMapper();
+  @Autowired
+  ItemRepository itemRepository;
+  @Autowired
+  ItemService itemService;
+
 
   @BeforeEach
   public void setUp() throws Exception {
     logger.info("Setup..");
-    BootStrapData bootStrapData = new BootStrapData(userRepository, userService);
+    BootStrapData bootStrapData = new BootStrapData(userRepository, userService, itemRepository, itemService);
     bootStrapData.run();
     userService = new UserServiceImpl(userRepository);
     userController = new UserController(userService);
-    mockMvc = MockMvcBuilders.standaloneSetup(userController)
+    mockMvc = MockMvcBuilders.standaloneSetup(userController, GlobalExceptionHandler.class)
       .build();
     user1 = new User.Builder().withId(1L)
       .withFirstName("John")
@@ -62,18 +75,34 @@ public class IntegrationTest {
       .build();
   }
 
-  public void tearDown() {
-    logger.info("Tear Down..");
-    userRepository.deleteAll();
+  @Test
+  void findAll_shouldReturnAllUsers() throws Exception {
+    User user = user1;
+    MvcResult result = mockMvc.perform(get(UserController.BASE_URL + "/all"))
+      .andExpect(status().isOk())
+      .andReturn();
+    List<User> actualUsers = objectMapper.readValue(result.getResponse()
+      .getContentAsString(), new TypeReference<>() {
+    });
+    assertThat(actualUsers.size()).isEqualTo(4);
+    assertThat(actualUsers.get(0)
+      .getId()).isEqualTo(user.getId());
+    assertThat(actualUsers.get(0)
+      .getFirstName()).isEqualTo(user1.getFirstName());
+    assertThat(actualUsers.get(0)
+      .getLastName()).isEqualTo(user1.getLastName());
+    assertThat(actualUsers.get(0)
+      .getEmail()).isEqualTo(user1.getEmail());
+    assertThat(actualUsers.get(0)
+      .getPassword()).isNull();
   }
 
   @Test
-  void shouldReturnSavedUser() throws Exception {
+  void shouldReturnUserById() throws Exception {
     User expectedUser = user1;
     MvcResult result = mockMvc.perform(get(UserController.BASE_URL + "/{id}", expectedUser.getId()))
       .andExpect(status().isOk())
       .andReturn();
-
     // Deserialize the response and check the user fields
     User actualUser = objectMapper.readValue(result.getResponse()
       .getContentAsString(), User.class);
@@ -90,7 +119,7 @@ public class IntegrationTest {
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     String json = objectMapper.writeValueAsString(user);
     // Perform the request and check the response status
-    MvcResult result = mockMvc.perform(post(UserController.BASE_URL + "/save").content(json)
+    MvcResult result = mockMvc.perform(post(UserController.BASE_URL).content(json)
         .contentType(MediaType.APPLICATION_JSON))
       .andExpect(status().isCreated())
       .andReturn();
