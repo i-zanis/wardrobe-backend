3775f976fcf1e440bd1ac84b0457b9e4a14211c9
i-zanis <ioanlafazanis@gmail.com>
2023-01-15 22:26:27 +0000

    chore: add Constants class

---
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/utils/Constants.java
@@ -0,0 +1,17 @@
+package io.noblackhole.wardrobe.wardrobebackend.utils;
+
+public class Constants {
+  public static final String EMPTY_STRING = "";
+  public static final String SPACE = " ";
+  public static final String COMMA = ",";
+  public static final String COLON = ":";
+  public static final String SEMICOLON = ";";
+  public static final String DOT = ".";
+  public static final String UNDERSCORE = "_";
+  public static final String SLASH = "/";
+  public static final String BACKSLASH = "\\";
+  public static final String DASH = "-";
+  public static final String PIPE = "|";
+  public static final String SINGLE_QUOTE = "'";
+  public static final String DOUBLE_QUOTE = "\"";
+}
