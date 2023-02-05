10c4a1f913302a91e98cf5af0e3a81c5dc207279
i-zanis <ioanlafazanis@gmail.com>
2023-02-05 22:10:17 +0000

    refactor: add overloaded constructors, change relationship in Look

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Look.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Look.java
@@ -1,16 +1,33 @@
 package io.noblackhole.wardrobe.wardrobebackend.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 import java.util.Set;
 
 @Entity
+@Table(name = "looks")
 public class Look extends BaseEntity {
-  @OneToMany
+  @ManyToMany
   Set<Item> items;
   private String name;
   private String description;
 
+  public Look(Set<Item> items, String name, String description) {
+    this.items = items;
+    this.name = name;
+    this.description = description;
+  }
+
+  public Look(Long id, Set<Item> items, String name, String description) {
+    super(id);
+    this.items = items;
+    this.name = name;
+    this.description = description;
+  }
+
+  public Look() {
+
+  }
 }
