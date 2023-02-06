3db8fba3dac57896474c1de8fcacd692b1b29780
i-zanis <ioanlafazanis@gmail.com>
2023-02-06 18:11:05 +0000

    feat: add Dto Mapper

---
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/dto/user/DtoMapper.java
@@ -0,0 +1,41 @@
+package io.noblackhole.wardrobe.wardrobebackend.domain.dto.user;
+
+import io.noblackhole.wardrobe.wardrobebackend.domain.Item;
+import io.noblackhole.wardrobe.wardrobebackend.domain.User;
+import io.noblackhole.wardrobe.wardrobebackend.domain.dto.item.ItemCreationDto;
+import io.noblackhole.wardrobe.wardrobebackend.domain.dto.item.ItemDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DtoMapper {
+  @Mapping(target = "preferences", source = "preferences")
+  UserDto userToUserDto(User user);
+
+  @Mapping(target = "preferences", source = "preferences")
+  User userDtoToUser(UserDto userDto);
+
+  @Mapping(target = "items", source = "items")
+  UserItemsDto userToUserItemsDto(User user);
+
+  @Mapping(target = "items", source = "items")
+  User userItemsDtoToUser(UserItemsDto userItemsDto);
+
+  @Mapping(target = "password", ignore = true)
+  User userCreationDtoToUser(UserCreationDto userCreationDto);
+
+  //  @Mapping(target = "password", source = "user.password")
+  UserCreationDto userToUserCreationDto(User user);
+
+
+  // *************************************************************
+  ItemCreationDto itemToItemCreationDto(Item item);
+
+  @Mapping(source = "userId", target = "user.id")
+  Item itemCreationDtoToItem(ItemCreationDto itemCreationDto);
+
+  @Mapping(source = "userId", target = "user.id")
+  Item itemDtoToItem(ItemDto itemDto);
+
+  ItemDto itemToItemDto(Item item);
+}
