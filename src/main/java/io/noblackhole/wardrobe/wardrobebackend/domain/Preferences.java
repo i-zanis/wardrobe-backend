e689e32dbf5c221a64c1a4a03ef2e85355c22029
i-zanis <ioanlafazanis@gmail.com>
2023-02-09 21:21:38 +0000

    refactor: update Preferences DarkMode, Notifications, Metric

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Preferences.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/Preferences.java
@@ -1,15 +1,62 @@
 package io.noblackhole.wardrobe.wardrobebackend.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 @Entity
 public class Preferences extends BaseEntity {
   String location;
-  @Column(name = "wear_type")
-  @Enumerated(value = EnumType.STRING)
-  WearType wearType;
-  Scale scale;
+  Boolean isDarkMode;
+  Boolean isMetric;
+  Boolean isNotificationsEnabled;
+
+  public Preferences(String location, Boolean isDarkMode, Boolean isMetric, Boolean isNotificationsEnabled) {
+    this.location = location;
+    this.isDarkMode = isDarkMode;
+    this.isMetric = isMetric;
+    this.isNotificationsEnabled = isNotificationsEnabled;
+  }
+
+  public Preferences(Long id, String location, Boolean isDarkMode, Boolean isMetric, Boolean isNotificationsEnabled) {
+    super(id);
+    this.location = location;
+    this.isDarkMode = isDarkMode;
+    this.isMetric = isMetric;
+    this.isNotificationsEnabled = isNotificationsEnabled;
+  }
+
+  public Preferences() {
+
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public Boolean getDarkMode() {
+    return isDarkMode;
+  }
+
+  public void setDarkMode(Boolean darkMode) {
+    isDarkMode = darkMode;
+  }
+
+  public Boolean getMetric() {
+    return isMetric;
+  }
+
+  public void setMetric(Boolean metric) {
+    isMetric = metric;
+  }
+
+  public Boolean getNotificationsEnabled() {
+    return isNotificationsEnabled;
+  }
+
+  public void setNotificationsEnabled(Boolean notificationsEnabled) {
+    isNotificationsEnabled = notificationsEnabled;
+  }
 }
