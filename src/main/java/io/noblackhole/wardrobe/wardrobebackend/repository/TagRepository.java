3d5d60ba23b5bd8bad7808d084729b1f7a154980
i-zanis <ioanlafazanis@gmail.com>
2023-02-26 13:52:10 +0000

    feat: add TagRepository

---
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package io.noblackhole.wardrobe.wardrobebackend.repository;
+
+import io.noblackhole.wardrobe.wardrobebackend.domain.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+}
