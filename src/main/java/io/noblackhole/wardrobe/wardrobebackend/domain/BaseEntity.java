0953fe96b5f7dd41f8dae94f31a847406bd7d420
i-zanis <ioanlafazanis@gmail.com>
2023-02-28 15:29:58 +0000

    refactor: change generation type to auto in BaseEntity

--- src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/BaseEntity.java
+++ src/main/java/io/noblackhole/wardrobe/wardrobebackend/domain/BaseEntity.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Positive
   private Long id;
 
