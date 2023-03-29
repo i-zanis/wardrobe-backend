a648883040e6a5f8e85135fe4fd8a8a8b34dc1ad
i-zanis <ioanlafazanis@gmail.com>
2023-03-29 19:12:52 +0100

    test(item): refactor ItemService tests to use the DTOs, check tags

--- src/test/java/io/noblackhole/wardrobe/wardrobebackend/service/ItemServiceTest.java
+++ src/test/java/io/noblackhole/wardrobe/wardrobebackend/service/ItemServiceTest.java
@@ -1,13 +1,19 @@
 package io.noblackhole.wardrobe.wardrobebackend.service;
 
-import io.noblackhole.wardrobe.wardrobebackend.TestItem;
+import io.noblackhole.wardrobe.wardrobebackend.TestItemCreationDto;
+import io.noblackhole.wardrobe.wardrobebackend.TestItemDto;
+import io.noblackhole.wardrobe.wardrobebackend.TestTag;
 import io.noblackhole.wardrobe.wardrobebackend.domain.Category;
-import io.noblackhole.wardrobe.wardrobebackend.domain.Color;
 import io.noblackhole.wardrobe.wardrobebackend.domain.Item;
+import io.noblackhole.wardrobe.wardrobebackend.domain.Tag;
 import io.noblackhole.wardrobe.wardrobebackend.domain.User;
-import io.noblackhole.wardrobe.wardrobebackend.exception.ItemNotFoundException;
-import io.noblackhole.wardrobe.wardrobebackend.exception.ItemServiceException;
+import io.noblackhole.wardrobe.wardrobebackend.domain.dto.item.ItemCreationDto;
+import io.noblackhole.wardrobe.wardrobebackend.domain.dto.item.ItemDto;
+import io.noblackhole.wardrobe.wardrobebackend.domain.dto.user.DtoMapper;
+import io.noblackhole.wardrobe.wardrobebackend.exception.item.ItemNotFoundException;
+import io.noblackhole.wardrobe.wardrobebackend.exception.item.ItemServiceException;
 import io.noblackhole.wardrobe.wardrobebackend.repository.ItemRepository;
+import io.noblackhole.wardrobe.wardrobebackend.repository.TagRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,18 +27,29 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ItemServiceTest {
+
   @Mock
   ItemRepository itemRepository;
+  @Mock
+  TagRepository tagRepository;
+  @Mock
+  DtoMapper itemDtoMapper;
+
   @InjectMocks
   ItemServiceImpl itemService;
+
   User user1;
-  Category category1, category2;
+  Category category1;
   Item item1, item2;
+  ItemDto itemDto1, itemDto2;
 
   @BeforeEach
   void setUp() {
@@ -40,15 +57,25 @@ class ItemServiceTest {
     item1 = new Item.Builder().withId(11L)
       .withBrand("Hallmark Devilson")
       .withCategory(category1)
-      .withColors(Set.of(Color.BLUE, Color.RED))
-      .withUser(user1)
+      .withColors(Set.of("Blue", "Red"))
       .build();
     item2 = new Item.Builder().withId(12L)
       .withBrand("Tristan's Fate")
       .withCategory(category1)
-      .withColors(Set.of(Color.PINK, Color.WHITE))
-      .withUser(user1)
+      .withColors(Set.of("Pink", "White"))
       .build();
+
+    itemDto1 = new ItemDto.Builder().withId(11L)
+      .withBrand("Hallmark Devilson")
+      .withCategory(category1)
+      .withColors(Set.of("Blue", "Red"))
+      .build();
+    itemDto2 = new ItemDto.Builder().withId(12L)
+      .withBrand("Tristan's Fate")
+      .withCategory(category1)
+      .withColors(Set.of("Pink", "White"))
+      .build();
+
     user1 = new User.Builder().withId(1L)
       .withFirstName("John")
       .withLastName("Doe")
@@ -60,85 +87,52 @@ class ItemServiceTest {
   @Test
   void findAllByUserId_whenNoItemsForUser_shouldReturnEmptyList() throws ItemServiceException {
     when(itemRepository.findAllByUserId(user1.getId())).thenReturn(Collections.emptyList());
-    List<Item> items = itemService.findAllByUserId(user1.getId());
+    List<ItemDto> items = itemService.findAllByUserId(user1.getId());
     assertThat(items).isEmpty();
   }
 
   @Test
   void findAllByUserId_whenMultipleUsers_shouldReturnCorrectItems() throws ItemServiceException {
-    User user2 = new User.Builder().withId(2L)
-      .build();
-    Item item3 = new Item.Builder().withId(13L)
-      .withUser(user2)
-      .build();
-    Item item4 = new Item.Builder().withId(14L)
-      .withUser(user2)
-      .build();
-
     when(itemRepository.findAllByUserId(user1.getId())).thenReturn(List.of(item1, item2));
-    when(itemRepository.findAllByUserId(user2.getId())).thenReturn(List.of(item3, item4));
+    when(itemDtoMapper.itemToItemDto(item1)).thenReturn(itemDto1);
+    when(itemDtoMapper.itemToItemDto(item2)).thenReturn(itemDto2);
 
-    List<Item> itemsForUser1 = itemService.findAllByUserId(user1.getId());
+    List<ItemDto> itemsForUser1 = itemService.findAllByUserId(user1.getId());
     assertThat(itemsForUser1).hasSize(2);
-    assertThat(itemsForUser1).containsExactly(item1, item2);
-
-    List<Item> itemsForUser2 = itemService.findAllByUserId(user2.getId());
-    assertThat(itemsForUser2).hasSize(2);
-    assertThat(itemsForUser2).containsExactly(item3, item4);
-  }
-
-  @Test
-  void findAllByUserId() throws ItemServiceException {
-    User user = user1;
-    Item personalItem1 = item1, personalItem2 = item2;
-    personalItem1.setUser(user);
-    personalItem2.setUser(user);
-    List<Item> personalItems = List.of(personalItem1, personalItem2);
-    when(itemRepository.findAllByUserId(user.getId())).thenReturn(personalItems);
-    List<Item> items = itemService.findAllByUserId(user.getId());
-    assertThat(items).hasSize(2);
+    assertThat(itemsForUser1).containsExactly(itemDto1, itemDto2);
   }
 
   @Test
   void findById() throws ItemServiceException, ItemNotFoundException {
-    User user = user1;
-    Item item = item1;
-    user.addItem(item);
-    item.setUser(user);
-    when(itemRepository.findById(item.getId())).thenReturn(Optional.of(item));
-    Item actualItem = itemService.findById(item.getId());
-    assertThat(user.getId()).isNotEqualTo(actualItem.getId());
-    assertThat(actualItem.getBrand()).isEqualTo(item.getBrand());
-    assertThat(actualItem.getNotes()).isEqualTo(item.getNotes());
-    assertThat(actualItem.getCategory()).isEqualTo(item.getCategory());
-    assertThat(actualItem.getColors()).isEqualTo(item.getColors());
-    assertThat(actualItem.getBrand()).isEqualTo(item.getBrand());
-    assertThat(actualItem.getUser()).isEqualTo(item.getUser());
+    when(itemRepository.findById(item1.getId())).thenReturn(Optional.of(item1));
+    when(itemDtoMapper.itemToItemDto(item1)).thenReturn(itemDto1);
+
+    ItemDto actualItem = itemService.findById(item1.getId());
+    assertThat(actualItem).isEqualTo(itemDto1);
   }
 
   @Test
   void save_validItem_shouldApply() throws ItemServiceException {
-    Item item = TestItem.create1();
-    when(itemRepository.save(item)).thenReturn(item);
-    itemService.save(item);
-    verify(itemRepository, times(1)).save(item);
-  }
+    ItemCreationDto itemCreationDto = TestItemCreationDto.create1();
 
-  @Test
-  void save_invalidItem_shouldFail() {
-    Item item = TestItem.create1();
-    item.setUser(null);
-    assertThatExceptionOfType(ItemServiceException.class)
-      .isThrownBy(() -> itemService.save(item));
-    verify(itemRepository, times(0)).save(item);
+    when(itemDtoMapper.itemCreationDtoToItem(itemCreationDto)).thenReturn(item1);
+    when(itemRepository.save(item1)).thenReturn(item1);
+    when(itemDtoMapper.itemToItemDto(item1)).thenReturn(itemDto1);
+
+    ItemDto savedItemDto = itemService.save(itemCreationDto);
+    assertThat(savedItemDto).isEqualTo(itemDto1);
+    verify(itemRepository, times(1)).save(item1);
   }
 
   @Test
   void update_validItem_shouldApply() throws ItemServiceException {
-    Item item = TestItem.create1();
-    when(itemRepository.save(item)).thenReturn(item);
-    itemService.update(item);
-    verify(itemRepository, times(1)).save(item);
+    when(itemDtoMapper.itemDtoToItem(itemDto1)).thenReturn(item1);
+    when(itemRepository.save(item1)).thenReturn(item1);
+    when(itemDtoMapper.itemToItemDto(item1)).thenReturn(itemDto1);
+
+    ItemDto updatedItemDto = itemService.update(itemDto1);
+    assertThat(updatedItemDto).isEqualTo(itemDto1);
+    verify(itemRepository, times(1)).save(item1);
   }
 
   @Test
@@ -151,12 +145,64 @@ class ItemServiceTest {
   }
 
   @Test
-  void update_invalidItem_shouldThrowException() {
-    Item item = TestItem.create1();
-    item.setUser(null);
-    assertThatExceptionOfType(ItemServiceException.class)
-      .isThrownBy(() -> itemService.update(item));
-    verify(itemRepository, times(0)).save(item);
+  void findById_notFound_shouldThrowException() {
+    Long itemId = 999L;
+    when(itemRepository.findById(itemId)).thenReturn(Optional.empty());
+
+    assertThrows(ItemNotFoundException.class,
+      () -> itemService.findById(itemId));
+    verify(itemRepository, times(1)).findById(itemId);
   }
 
+  @Test
+  void save_withExistingTags_shouldReuseTags() throws ItemServiceException {
+    Tag tag1 = TestTag.create1();
+    Tag tag2 = TestTag.create2();
+    ItemCreationDto itemCreationDto = TestItemCreationDto.create1();
+
+    when(tagRepository.findByName(tag1.getName())).thenReturn(tag1);
+    when(tagRepository.findByName(tag2.getName())).thenReturn(tag2);
+
+    Item item = new Item();
+    item.setTags(Set.of(tag1, tag2));
+    when(itemDtoMapper.itemCreationDtoToItem(itemCreationDto)).thenReturn(item);
+    when(itemRepository.save(item)).thenReturn(item);
+
+    ItemDto itemDto = TestItemDto.create1();
+    when(itemDtoMapper.itemToItemDto(item)).thenReturn(itemDto);
+
+    ItemDto savedItemDto = itemService.save(itemCreationDto);
+
+    assertThat(savedItemDto).isEqualTo(itemDto);
+    verify(tagRepository, times(1)).findByName(tag1.getName());
+    verify(tagRepository, times(1)).findByName(tag2.getName());
+    verify(itemRepository, times(1)).save(item);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
